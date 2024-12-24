d50f311f464bbd403256d75d1906a2ac2cf1eb97
Tim <zhangwei2090@gmail.com>
2024-12-24 17:50:19 +0800

    add: ci

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,15 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  // build: {
-  //   outDir: 'docs',
-  // },
+  // 构建产物平铺
+  build: {
+    rollupOptions: {
+      output: {
+        // 让所有 JS 和 CSS 文件平铺在 dist 目录下
+        entryFileNames: '[name].js', // 入口文件 JS 使用文件名作为文件名
+        chunkFileNames: '[name].js', // 代码拆分生成的 chunk 也平铺
+        assetFileNames: '[name].[ext]', // 其他资源（例如图片、字体等）直接放在 dist 目录下，保持原有文件名
+      },
+    },
+  },
 })
